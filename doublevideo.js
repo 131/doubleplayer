9e738007184b36d6fefd49fd763e43c321985b23
François Leurent <131.php@cloudyks.org>
2016-07-12 18:43:11 +0200

    Missing udom module + misc cleanup

--- doublevideo.js
+++ doublevideo.js
@@ -2,35 +2,33 @@
 "use strict";
 
 var shaderFs = `
-precision mediump float;
-uniform sampler2D sm;
-varying vec2 tx;
+  precision mediump float;
+  uniform sampler2D sm;
+  varying vec2 tx;
 
-void main(void) {
-  gl_FragColor=texture2D(sm,tx);
-}
+  void main(void) {
+    gl_FragColor=texture2D(sm,tx);
+  }
 `;
 
 var shaderVs = `
-attribute vec2 vx;
-varying vec2 tx;
-uniform float xFactor;
-uniform float imgFac;
+  attribute vec2 vx;
+  varying vec2 tx;
+  uniform float xFactor;
+  uniform float imgFac;
 
 
-void main(void) {
-  float vxx = vx.x * ( 1.0 - xFactor) + xFactor;
-  gl_Position=vec4((vx.x*2.0*xFactor-1.0)*imgFac + (1.0-imgFac)*(vxx*2.0-1.0)  , 1.0-vx.y*2.0, 0 , 1);
-  tx=vec2((vx.x*xFactor/2.0)*imgFac + (1.0-imgFac)*(vxx / 2.0 + 0.5) , vx.y);
-}
+  void main(void) {
+    float vxx   = vx.x * ( 1.0 - xFactor) + xFactor;
+    gl_Position = vec4((vx.x*2.0*xFactor-1.0)*imgFac + (1.0-imgFac)*(vxx*2.0-1.0)  , 1.0-vx.y*2.0, 0 , 1);
+    tx          = vec2((vx.x*xFactor/2.0)*imgFac + (1.0-imgFac)*(vxx / 2.0 + 0.5) , vx.y);
+  }
 `;
 
 
 var Class       = require('uclass');
 var $n          = require('udom/element/create');
 var requestAnimationFrame = require('udom/window/requestAnimationFrame');
-var errcnt = 0;
-var videoready = true;
 
 var DoubleVideo = new Class({
   Binds : ['prepareCanvas', 'timerCallback', 'computeFrame' , 'initWebGL' , 'initBuffers' , 'initShaders'],
@@ -61,9 +59,6 @@ var DoubleVideo = new Class({
 
     video.addEventListener("loadedmetadata", function(){
 
-        video.setAttribute("playsinline", "");
-    		video.setAttribute("webkit-playsinline", "");
-
         if(ready){
           console.log('META DATA ALREADY LOADED !');
           return;
@@ -79,10 +74,11 @@ var DoubleVideo = new Class({
        self.video_width = video.videoWidth;
        self.video_height = video.videoHeight;
 
-       self.prepareCanvas();
-       self.initWebGL();
-       self.initShaders();
-       self.initBuffers();
+       var canvas = self.prepareCanvas();
+       var gl = self.initWebGL(canvas);
+       self.initShaders(gl);
+       self.initBuffers(gl);
+       self.gl = gl;
        self.timerCallback();
 
 
@@ -97,9 +93,8 @@ var DoubleVideo = new Class({
     });
   },
 
-  initShaders : function () {
+  initShaders : function(gl) {
     var self = this;
-    var gl =  self.gl;
 
 
     var fragmentShader = this.getShader(gl, shaderFs, gl.FRAGMENT_SHADER);
@@ -112,17 +107,14 @@ var DoubleVideo = new Class({
     gl.linkProgram(self.shaderProgram);
 
 
-    if (!gl.getProgramParameter(self.shaderProgram, gl.LINK_STATUS)) {
-      alert("Unable to initialize the shader program: " + gl.getProgramInfoLog(  self.shaderProgram ));
-    }
+    if (!gl.getProgramParameter(self.shaderProgram, gl.LINK_STATUS))
+      throw ("Unable to initialize the shader program: " + gl.getProgramInfoLog(  self.shaderProgram ));
 
     gl.useProgram(self.shaderProgram);
-
 
     self.vx_ptr = gl.getAttribLocation(self.shaderProgram, "vx");
     gl.enableVertexAttribArray(self.vx_ptr);
     gl.uniform1i(gl.getUniformLocation(self.shaderProgram, "sm"), 0);
-
   },
 
 
@@ -144,11 +136,9 @@ var DoubleVideo = new Class({
   },
 
 
-  initBuffers: function() {
+  initBuffers: function(gl) {
 
     // Create a buffer for the square's vertices.
-    var gl =  this.gl;
-
     this.vx = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, this.vx);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([0,0, 1,0, 1,1, 0,1]), gl.STATIC_DRAW);
@@ -158,61 +148,45 @@ var DoubleVideo = new Class({
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array([0,1,2, 0,2,3]), gl.STATIC_DRAW);
 
 
-  	this.tex = gl.createTexture();
-  	gl.bindTexture(gl.TEXTURE_2D, this.tex);
-  	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T,     gl.CLAMP_TO_EDGE);
-  	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S,     gl.CLAMP_TO_EDGE);
-  	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-  	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-
+    this.tex = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, this.tex);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T,     gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S,     gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
   },
 
 
-  initWebGL: function(){
-    var self = this;
-    self.gl = null;
+  initWebGL: function(canvas){
+    var gl = canvas.getContext("experimental-webgl");
 
-    try {
-      self.gl = self.canvas.getContext("experimental-webgl");
-    }
-    catch(e) {
-    }
-    // If we don't have a GL context, give up now
-    if (!self.gl) {
-      return alert("Unable to initialize WebGL. Your browser may not support it.");
-    }
-
-
-    if (self.gl) {
-      self.gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
-      self.gl.clearDepth(1.0);                 // Clear everything
-      self.gl.enable(self.gl.DEPTH_TEST);           // Enable depth testing
-      self.gl.depthFunc(self.gl.LEQUAL);            // Near things obscure far thingsablished.
-
-
-    }
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clearDepth(1.0);
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
+    return gl;
   },
 
   prepareCanvas: function() {
     var self = this;
-    self.canvas = $n('canvas', {width:self.video_width / 2, height:self.video_height});
+    var canvas = $n('canvas', {width:self.video_width / 2, height:self.video_height});
 
-    self.canvas.style.width = self.canvas.style.height = '100%';
+    canvas.style.width = canvas.style.height = '100%';
 
-    self.container.appendChild(self.canvas);
+    self.container.appendChild(canvas);
 
     self.delimiter = 0.5;
-    self.canvas.addEventListener('mousemove', function(e){
+    canvas.addEventListener('mousemove', function(e){
       var x = e.clientX;
       x =  x / self.container.offsetWidth;
       self.delimiter = x
     }, false);
 
-    self.canvas.addEventListener('touchstart', function(e) {
+    canvas.addEventListener('touchstart', function(e) {
       //  nothing
     });
 
-    self.canvas.addEventListener('touchmove', function(e){
+    canvas.addEventListener('touchmove', function(e){
       var x = e.touches[0].clientX;
       x = x / self.container.offsetWidth;
 
@@ -223,6 +197,7 @@ var DoubleVideo = new Class({
 
     self.uTimeout = Math.random();
     window.globalTimeout = self.uTimeout;
+    return canvas;
   },
 
 
@@ -244,23 +219,8 @@ var DoubleVideo = new Class({
     gl.clear(gl.COLOR_BUFFER_BIT);
     gl.activeTexture(gl.TEXTURE0);
     gl.bindTexture(gl.TEXTURE_2D, self.tex);
-
-
-      try
-      {
-        // upload the video frame
-        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, this.video);
-      }
-      catch(e)
-      {
-        // log only the first few errors
-        errcnt++;
-        if (errcnt < 10)
-          console.log(e, "color:red;");
-        else if (errcnt == 10)
-          console.log("...", "color:red;");
-      }
-
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, this.video);
+   
 
     var half = (self.video_width /2);
     var x = Math.max( 1, Math.min(half -1,   half * self.delimiter));
@@ -292,20 +252,6 @@ var DoubleVideo = new Class({
 
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, self.ix);
     gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
-
-
-
-
-
-
-
-    //var frame1 = self.canvas_buffer.getImageData(0, 0, x, self.video_height);
-    //var frame2 = self.canvas_buffer.getImageData(half + x , 0, half - x, self.video_height);
-
-    //self.canvas_ctx.putImageData(frame1, 0, 0);
-    //self.canvas_ctx.putImageData(frame2, x, 0);
-  //  self.canvas_ctx.fillRect (x, 0, 2, self.video_height);
-
   }
 
 });
--- release/doubleplayer.js
+++ release/doubleplayer.js
@@ -16,257 +16,224 @@ module.exports = DoublePlayer;
 
 "use strict";
 
-var shaderFs = '\nprecision mediump float;\nuniform sampler2D sm;\nvarying vec2 tx;\n\nvoid main(void) {\n  gl_FragColor=texture2D(sm,tx);\n}\n';
+var shaderFs = '\n  precision mediump float;\n  uniform sampler2D sm;\n  varying vec2 tx;\n\n  void main(void) {\n    gl_FragColor=texture2D(sm,tx);\n  }\n';
 
-var shaderVs = '\nattribute vec2 vx;\nvarying vec2 tx;\nuniform float xFactor;\nuniform float imgFac;\n\n\nvoid main(void) {\n  float vxx = vx.x * ( 1.0 - xFactor) + xFactor;\n  gl_Position=vec4((vx.x*2.0*xFactor-1.0)*imgFac + (1.0-imgFac)*(vxx*2.0-1.0)  , 1.0-vx.y*2.0, 0 , 1);\n  tx=vec2((vx.x*xFactor/2.0)*imgFac + (1.0-imgFac)*(vxx / 2.0 + 0.5) , vx.y);\n}\n';
+var shaderVs = '\n  attribute vec2 vx;\n  varying vec2 tx;\n  uniform float xFactor;\n  uniform float imgFac;\n\n\n  void main(void) {\n    float vxx   = vx.x * ( 1.0 - xFactor) + xFactor;\n    gl_Position = vec4((vx.x*2.0*xFactor-1.0)*imgFac + (1.0-imgFac)*(vxx*2.0-1.0)  , 1.0-vx.y*2.0, 0 , 1);\n    tx          = vec2((vx.x*xFactor/2.0)*imgFac + (1.0-imgFac)*(vxx / 2.0 + 0.5) , vx.y);\n  }\n';
 
 var Class = require('uclass');
 var $n = require('udom/element/create');
 var requestAnimationFrame = require('udom/window/requestAnimationFrame');
-var errcnt = 0;
-var videoready = true;
 
 var DoubleVideo = new Class({
-    Binds: ['prepareCanvas', 'timerCallback', 'computeFrame', 'initWebGL', 'initBuffers', 'initShaders'],
+  Binds: ['prepareCanvas', 'timerCallback', 'computeFrame', 'initWebGL', 'initBuffers', 'initShaders'],
 
-    container: null,
-    interval: null,
+  container: null,
+  interval: null,
 
-    initialize: function initialize(video_url, container) {
+  initialize: function initialize(video_url, container) {
 
-        var self = this,
-            video = $n('video', { loop: true, autoplay: true });
-        console.log('Video src is : ', video_url);
+    var self = this,
+        video = $n('video', { loop: true, autoplay: true });
+    console.log('Video src is : ', video_url);
 
-        this.container = container;
-        var videos = container.getElementsByTagName('video');
+    this.container = container;
+    var videos = container.getElementsByTagName('video');
 
-        //pause current video (if any) before looping
-        if (videos.length > 0) {
-            videos[0].pause();
-            videos[0].src = '';
-        }
-        video.src = '';
-
-        this.container.innerHTML = "";
-
-        var ready = false;
-
-        video.addEventListener("loadedmetadata", function () {
-
-            video.setAttribute("playsinline", "");
-            video.setAttribute("webkit-playsinline", "");
-
-            if (ready) {
-                console.log('META DATA ALREADY LOADED !');
-                return;
-            }
-
-            console.log("Loadedd metadata");
-
-            ready = true;
-
-            //video.play();
-            self.video = video;
-            self.video.id = "Myvideo";
-            self.video_width = video.videoWidth;
-            self.video_height = video.videoHeight;
-
-            self.prepareCanvas();
-            self.initWebGL();
-            self.initShaders();
-            self.initBuffers();
-            self.timerCallback();
-        });
-
-        //leave this to next tick
-        requestAnimationFrame(function () {
-            video.src = video_url + "#" + Math.random();
-            //alert(video.src);
-            video.play();
-        });
-    },
-
-    initShaders: function initShaders() {
-        var self = this;
-        var gl = self.gl;
-
-        var fragmentShader = this.getShader(gl, shaderFs, gl.FRAGMENT_SHADER);
-        var vertexShader = this.getShader(gl, shaderVs, gl.VERTEX_SHADER);
-
-        self.shaderProgram = gl.createProgram();
-        gl.attachShader(self.shaderProgram, vertexShader);
-        gl.attachShader(self.shaderProgram, fragmentShader);
-        gl.linkProgram(self.shaderProgram);
-
-        if (!gl.getProgramParameter(self.shaderProgram, gl.LINK_STATUS)) {
-            alert("Unable to initialize the shader program: " + gl.getProgramInfoLog(self.shaderProgram));
-        }
-
-        gl.useProgram(self.shaderProgram);
-
-        self.vx_ptr = gl.getAttribLocation(self.shaderProgram, "vx");
-        gl.enableVertexAttribArray(self.vx_ptr);
-        gl.uniform1i(gl.getUniformLocation(self.shaderProgram, "sm"), 0);
-    },
-
-    getShader: function getShader(gl, theSource, type) {
-
-        var shader = gl.createShader(type);
-
-        gl.shaderSource(shader, theSource);
-
-        gl.compileShader(shader);
-
-        if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-            alert("An error occurred compiling the shaders: " + gl.getShaderInfoLog(shader));
-            return null;
-        }
-
-        return shader;
-    },
-
-    initBuffers: function initBuffers() {
-
-        // Create a buffer for the square's vertices.
-        var gl = this.gl;
-
-        this.vx = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.vx);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([0, 0, 1, 0, 1, 1, 0, 1]), gl.STATIC_DRAW);
-
-        this.ix = gl.createBuffer();
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.ix);
-        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array([0, 1, 2, 0, 2, 3]), gl.STATIC_DRAW);
-
-        this.tex = gl.createTexture();
-        gl.bindTexture(gl.TEXTURE_2D, this.tex);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-    },
-
-    initWebGL: function initWebGL() {
-        var self = this;
-        self.gl = null;
-
-        try {
-            self.gl = self.canvas.getContext("experimental-webgl");
-        } catch (e) {}
-        // If we don't have a GL context, give up now
-        if (!self.gl) {
-            return alert("Unable to initialize WebGL. Your browser may not support it.");
-        }
-
-        if (self.gl) {
-            self.gl.clearColor(0.0, 0.0, 0.0, 1.0); // Clear to black, fully opaque
-            self.gl.clearDepth(1.0); // Clear everything
-            self.gl.enable(self.gl.DEPTH_TEST); // Enable depth testing
-            self.gl.depthFunc(self.gl.LEQUAL); // Near things obscure far thingsablished.
-        }
-    },
-
-    prepareCanvas: function prepareCanvas() {
-        var self = this;
-        self.canvas = $n('canvas', { width: self.video_width / 2, height: self.video_height });
-
-        self.canvas.style.width = self.canvas.style.height = '100%';
-
-        self.container.appendChild(self.canvas);
-
-        self.delimiter = 0.5;
-        self.canvas.addEventListener('mousemove', function (e) {
-            var x = e.clientX;
-            x = x / self.container.offsetWidth;
-            self.delimiter = x;
-        }, false);
-
-        self.canvas.addEventListener('touchstart', function (e) {
-            //  nothing
-        });
-
-        self.canvas.addEventListener('touchmove', function (e) {
-            var x = e.touches[0].clientX;
-            x = x / self.container.offsetWidth;
-
-            self.delimiter = x;
-        }, false);
-
-        self.uTimeout = Math.random();
-        window.globalTimeout = self.uTimeout;
-    },
-
-    timerCallback: function timerCallback() {
-        var self = this;
-
-        if (self.uTimeout != window.globalTimeout) {
-            console.log("//kthxby");
-            return;
-        }
-
-        self.computeFrame();
-        requestAnimationFrame(self.timerCallback);
-    },
-
-    computeFrame: function computeFrame() {
-        var self = this;
-        var gl = self.gl;
-        gl.clear(gl.COLOR_BUFFER_BIT);
-        gl.activeTexture(gl.TEXTURE0);
-        gl.bindTexture(gl.TEXTURE_2D, self.tex);
-
-        try {
-            // upload the video frame
-            gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, this.video);
-        } catch (e) {
-            // log only the first few errors
-            errcnt++;
-            if (errcnt < 10) console.log(e, "color:red;");else if (errcnt == 10) console.log("...", "color:red;");
-        }
-
-        var half = self.video_width / 2;
-        var x = Math.max(1, Math.min(half - 1, half * self.delimiter));
-
-        var xPos = x / self.video_width * 2;
-
-        var xUniform = gl.getUniformLocation(self.shaderProgram, "xFactor");
-        gl.uniform1f(xUniform, xPos);
-
-        var xUniform = gl.getUniformLocation(self.shaderProgram, "imgFac");
-        gl.uniform1f(xUniform, 0);
-
-        gl.bindBuffer(gl.ARRAY_BUFFER, self.vx);
-        gl.vertexAttribPointer(self.vx_ptr, 2, gl.FLOAT, false, 0, 0);
-
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, self.ix);
-        gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
-
-        var xUniform = gl.getUniformLocation(self.shaderProgram, "xFactor");
-        gl.uniform1f(xUniform, xPos);
-
-        var xUniform = gl.getUniformLocation(self.shaderProgram, "imgFac");
-        gl.uniform1f(xUniform, 1);
-
-        gl.bindBuffer(gl.ARRAY_BUFFER, self.vx);
-        gl.vertexAttribPointer(self.vx_ptr, 2, gl.FLOAT, false, 0, 0);
-
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, self.ix);
-        gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
-
-        //var frame1 = self.canvas_buffer.getImageData(0, 0, x, self.video_height);
-        //var frame2 = self.canvas_buffer.getImageData(half + x , 0, half - x, self.video_height);
-
-        //self.canvas_ctx.putImageData(frame1, 0, 0);
-        //self.canvas_ctx.putImageData(frame2, x, 0);
-        //  self.canvas_ctx.fillRect (x, 0, 2, self.video_height);
+    //pause current video (if any) before looping
+    if (videos.length > 0) {
+      videos[0].pause();
+      videos[0].src = '';
     }
+    video.src = '';
+
+    this.container.innerHTML = "";
+
+    var ready = false;
+
+    video.addEventListener("loadedmetadata", function () {
+
+      if (ready) {
+        console.log('META DATA ALREADY LOADED !');
+        return;
+      }
+
+      console.log("Loadedd metadata");
+
+      ready = true;
+
+      //video.play();
+      self.video = video;
+      self.video.id = "Myvideo";
+      self.video_width = video.videoWidth;
+      self.video_height = video.videoHeight;
+
+      var canvas = self.prepareCanvas();
+      var gl = self.initWebGL(canvas);
+      self.initShaders(gl);
+      self.initBuffers(gl);
+      self.gl = gl;
+      self.timerCallback();
+    });
+
+    //leave this to next tick
+    requestAnimationFrame(function () {
+      video.src = video_url + "#" + Math.random();
+      //alert(video.src);
+      video.play();
+    });
+  },
+
+  initShaders: function initShaders(gl) {
+    var self = this;
+
+    var fragmentShader = this.getShader(gl, shaderFs, gl.FRAGMENT_SHADER);
+    var vertexShader = this.getShader(gl, shaderVs, gl.VERTEX_SHADER);
+
+    self.shaderProgram = gl.createProgram();
+    gl.attachShader(self.shaderProgram, vertexShader);
+    gl.attachShader(self.shaderProgram, fragmentShader);
+    gl.linkProgram(self.shaderProgram);
+
+    if (!gl.getProgramParameter(self.shaderProgram, gl.LINK_STATUS)) throw "Unable to initialize the shader program: " + gl.getProgramInfoLog(self.shaderProgram);
+
+    gl.useProgram(self.shaderProgram);
+
+    self.vx_ptr = gl.getAttribLocation(self.shaderProgram, "vx");
+    gl.enableVertexAttribArray(self.vx_ptr);
+    gl.uniform1i(gl.getUniformLocation(self.shaderProgram, "sm"), 0);
+  },
+
+  getShader: function getShader(gl, theSource, type) {
+
+    var shader = gl.createShader(type);
+
+    gl.shaderSource(shader, theSource);
+
+    gl.compileShader(shader);
+
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+      alert("An error occurred compiling the shaders: " + gl.getShaderInfoLog(shader));
+      return null;
+    }
+
+    return shader;
+  },
+
+  initBuffers: function initBuffers(gl) {
+
+    // Create a buffer for the square's vertices.
+    this.vx = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.vx);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([0, 0, 1, 0, 1, 1, 0, 1]), gl.STATIC_DRAW);
+
+    this.ix = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.ix);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array([0, 1, 2, 0, 2, 3]), gl.STATIC_DRAW);
+
+    this.tex = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, this.tex);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+  },
+
+  initWebGL: function initWebGL(canvas) {
+    var gl = canvas.getContext("experimental-webgl");
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clearDepth(1.0);
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
+    return gl;
+  },
+
+  prepareCanvas: function prepareCanvas() {
+    var self = this;
+    var canvas = $n('canvas', { width: self.video_width / 2, height: self.video_height });
+
+    canvas.style.width = canvas.style.height = '100%';
+
+    self.container.appendChild(canvas);
+
+    self.delimiter = 0.5;
+    canvas.addEventListener('mousemove', function (e) {
+      var x = e.clientX;
+      x = x / self.container.offsetWidth;
+      self.delimiter = x;
+    }, false);
+
+    canvas.addEventListener('touchstart', function (e) {
+      //  nothing
+    });
+
+    canvas.addEventListener('touchmove', function (e) {
+      var x = e.touches[0].clientX;
+      x = x / self.container.offsetWidth;
+
+      self.delimiter = x;
+    }, false);
+
+    self.uTimeout = Math.random();
+    window.globalTimeout = self.uTimeout;
+    return canvas;
+  },
+
+  timerCallback: function timerCallback() {
+    var self = this;
+
+    if (self.uTimeout != window.globalTimeout) {
+      console.log("//kthxby");
+      return;
+    }
+
+    self.computeFrame();
+    requestAnimationFrame(self.timerCallback);
+  },
+
+  computeFrame: function computeFrame() {
+    var self = this;
+    var gl = self.gl;
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, self.tex);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, this.video);
+
+    var half = self.video_width / 2;
+    var x = Math.max(1, Math.min(half - 1, half * self.delimiter));
+
+    var xPos = x / self.video_width * 2;
+
+    var xUniform = gl.getUniformLocation(self.shaderProgram, "xFactor");
+    gl.uniform1f(xUniform, xPos);
+
+    var xUniform = gl.getUniformLocation(self.shaderProgram, "imgFac");
+    gl.uniform1f(xUniform, 0);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, self.vx);
+    gl.vertexAttribPointer(self.vx_ptr, 2, gl.FLOAT, false, 0, 0);
+
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, self.ix);
+    gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
+
+    var xUniform = gl.getUniformLocation(self.shaderProgram, "xFactor");
+    gl.uniform1f(xUniform, xPos);
+
+    var xUniform = gl.getUniformLocation(self.shaderProgram, "imgFac");
+    gl.uniform1f(xUniform, 1);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, self.vx);
+    gl.vertexAttribPointer(self.vx_ptr, 2, gl.FLOAT, false, 0, 0);
+
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, self.ix);
+    gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
+  }
 
 });
 
 module.exports = DoubleVideo;
 
-},{"uclass":15,"udom/element/create":16,"udom/window/requestAnimationFrame":17}],3:[function(require,module,exports){
+},{"uclass":15,"udom/element/create":16,"udom/window/requestAnimationFrame":19}],3:[function(require,module,exports){
 var kindOf = require('./kindOf');
 var isPlainObject = require('./isPlainObject');
 var mixIn = require('../object/mixIn');
@@ -755,7 +722,11 @@ module.exports = function(tagname, attrs){
 
   return foo;
 }
-},{"mout/object/forIn":10}],17:[function(require,module,exports){
+},{"mout/object/forIn":17}],17:[function(require,module,exports){
+arguments[4][10][0].apply(exports,arguments)
+},{"./hasOwn":18,"dup":10}],18:[function(require,module,exports){
+arguments[4][12][0].apply(exports,arguments)
+},{"dup":12}],19:[function(require,module,exports){
 "use strict";
 
 module.exports = (function(){
@@ -767,9 +738,9 @@ module.exports = (function(){
           };
 })();
 
-},{}],18:[function(require,module,exports){
+},{}],20:[function(require,module,exports){
 'use strict';
 
 window.DoublePlayer = require('..');
 
-},{"..":1}]},{},[18]);
+},{"..":1}]},{},[20]);
