d9baa7afca2399d17b55e6d1eee75e0f6ef7d0ff
Mejdi Ourir <m.ourir@ivsweb.com>
2019-03-26 17:07:13 +0100

    corect releas no websocket

--- release/index.js
+++ release/index.js
@@ -1,14 +1,4 @@
-(function (host, port) {
-  var sock = new WebSocket('ws://' + host + ':' + port);
-  sock.onopen = function() {
-    console.log("Connected to browserify-reload handle");
-  }
-
-  sock.onmessage = function(msg){
-    if(msg && msg.data == "reload")
-      document.location = document.location.href;
-  }
-})(document.location.hostname, 59369);(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 'use strict';
 
 window.DoublePlayer = require('./');
